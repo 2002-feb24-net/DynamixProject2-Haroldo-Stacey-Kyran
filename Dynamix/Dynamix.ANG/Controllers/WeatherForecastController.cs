a567213cc83bacc835664fcac2726601e798e5a5
Kyran <k.b_bryant@icloud.com>
2020-04-11 19:22:24 -0500

    re-organized the projects so that we have  an angular project and an API project.

--- Dynamix/Dynamix.ANG/Controllers/WeatherForecastController.cs
+++ Dynamix/Dynamix.ANG/Controllers/WeatherForecastController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
-namespace Dynamix.WebAPI.Controllers
+namespace Dynamix.API.Controllers
 {
     [ApiController]
     [Route("[controller]")]
--- Dynamix/Dynamix.ANG/Program.cs
+++ Dynamix/Dynamix.ANG/Program.cs
@@ -7,12 +7,14 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Dynamix.WebAPI
+namespace Dynamix.API
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+
+            //this is the ninja theme
             CreateHostBuilder(args).Build().Run();
         }
 
