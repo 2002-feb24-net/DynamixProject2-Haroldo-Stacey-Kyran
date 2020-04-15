dd7a4f1fbf3f1ba1c875666cad72736543692ba4
Kyran <k.b_bryant@icloud.com>
2020-04-14 21:35:00 -0500

    added back program.cs

--- Dynamix/Dynamix.RestAPI/Program.cs
+++ Dynamix/Dynamix.RestAPI/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Dynamix.Client
+namespace Dynamix.WebAPI
 {
     //Stacey was here
     public class Program
