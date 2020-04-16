6cc5aacc53a4ee5b74737444740e64bfbf48cf48
Haroldoa <haroldo.j.altamirano@biola.edu>
2020-04-15 22:24:28 -0500

    added base repo classes. Need to integrate them in the general repo class

---
+++ Dynamix/Dynamix.RestAPI/Repositories/LocationRepository.cs
@@ -0,0 +1,17 @@
+﻿using Dynamix.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dynamix.API.Repositories
+{
+    public class LocationRepository : Repository<Location>
+    {
+        DbDynamixContext context;
+        public LocationRepository(DbDynamixContext context)
+        {
+            this.context = context;
+        }
+    }
+}
